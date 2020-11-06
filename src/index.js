1e3aa2588b17a42ee6964493c3eb07693369a3fd
kbrnsr <1452229+kbrnsr@users.noreply.github.com>
2020-11-06 10:32:11 +0100

    Added Hello app Added npm package tachyons
    Changed initialization to run Hello with argument

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+ReactDOM.render(<Hello greeting={'Hello' + 'React Ninja'}/>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
