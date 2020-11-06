09450f639f3bafb8dd48afcbb894d4f0246fc5e4
kbrnsr <1452229+kbrnsr@users.noreply.github.com>
2020-11-06 16:40:45 +0100

    Added Scroll component and modified main app to contain CardList component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import Searchbox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
@@ -37,7 +38,9 @@ class App extends Component {
         <div className="tc">
           <h1 className="f1">RoboFriends</h1>
           <Searchbox searchChange={ this.onChangeSearch } />
-          <CardList robots={ filteredRobots }/>
+          <Scroll>
+            <CardList robots={ filteredRobots }/>
+          </Scroll>
         </div>
       );
     }
