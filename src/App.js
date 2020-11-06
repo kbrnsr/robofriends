62b4133af76393ce0577cabdc6977bd644986118
kbrnsr <1452229+kbrnsr@users.noreply.github.com>
2020-11-06 16:20:01 +0100

    Modified to use rest api for user population

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import Searchbox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ""
     }
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => this.setState( {robots: users} ));
   }
 
   onChangeSearch = (event) => {
@@ -21,13 +26,21 @@ class App extends Component {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <Searchbox searchChange={ this.onChangeSearch } />
-        <CardList robots={ filteredRobots }/>
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return (
+        <div className="tc">
+          <h1 className="f1">Loading</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <Searchbox searchChange={ this.onChangeSearch } />
+          <CardList robots={ filteredRobots }/>
+        </div>
+      );
+    }
   }
 }
 
