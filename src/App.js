cef6a7371434b3d8897a8537e7308e3d21b2af43
kbrnsr <1452229+kbrnsr@users.noreply.github.com>
2020-11-06 15:44:16 +0100

    Added styling to main app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import CardList from './CardList';
 import Searchbox from './SearchBox';
 import { robots } from './robots';
+import './App.css';
+
 class App extends Component {
   constructor() {
     super();
@@ -21,7 +23,7 @@ class App extends Component {
     });
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
+        <h1 className="f1">RoboFriends</h1>
         <Searchbox searchChange={ this.onChangeSearch } />
         <CardList robots={ filteredRobots }/>
       </div>
